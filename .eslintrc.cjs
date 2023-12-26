6310b448f3f467856d6dc879db5d77d05ae509fa
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2023-12-26 16:14:14 +0530

    eslint config done.

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended-type-checked",
+        "prettier",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+    },
+    plugins: ["@typescript-eslint"],
+    root: true,
+};
